36871965554fc33f15a6005957e770da8f452c66
RohanAwate9020 <awaterohan9535@gmail.com>
2025-03-09 16:25:18 +0530

    implemented sorting on rating and price

--- frontend/src/features/product/component/ProductList.jsx
+++ frontend/src/features/product/component/ProductList.jsx
@@ -194,28 +194,34 @@ export default function ProductList() {
   const products = useSelector(selectAllProducts);
   const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
   const [filter, setFilter] = useState({});
+  const [sort, setSort] = useState({});
 
   const handleFilter = (e, section, option) => {
     const newFilter = { ...filter };
     if (e.target.checked) {
-      newFilter[section.id] = option.value;
+      if (newFilter[section.id]) {
+      newFilter[section.id].push(option.value);
+      }
+      else{
+        newFilter[section.id] = [option.value];
+      }
     } else {
-      delete newFilter[section.id];
+      const index= newFilter[section.id].findIndex(el => el ===option.value);
+      newFilter[section.id].splice(index,1)
     }
 
+    console.log(newFilter);
     setFilter(newFilter);
-    console.log(section.id);
   };
 
   const handleSort = (e, option) => {
-    const newFilter = { ...filter, _sort: option.sort, _order: option.order };
-    setFilter(newFilter); // Update state
-    dispatch(fetchProductsByFiltersAsync(filter));
+    const newSort = {_sort: option.sort, _order: option.order };
+    setSort(newSort); // Update state
   };
 
   useEffect(() => {
-    dispatch(fetchProductsByFiltersAsync(filter));
-  }, [dispatch,filter]);
+    dispatch(fetchProductsByFiltersAsync({filter, sort}));
+  }, [dispatch, filter ,sort]);
 
   return (
     <div>
--- frontend/src/features/product/productAPI.js
+++ frontend/src/features/product/productAPI.js
@@ -1,34 +1,44 @@
 export function fetchAllProducts() {
-    return new Promise(async (resolve) =>{
-      const response = await fetch('http://localhost:8080/products') 
-      const data = await response.json()
-      resolve({data})
-    }
-    );
-  }
-export function fetchProductsByFilters(filter) {
-  let querystring="";
+  return new Promise(async (resolve) => {
+    const response = await fetch("http://localhost:8080/products");
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+export function fetchProductsByFilters(filter, sort) {
+  let querystring = "";
   for (let key in filter) {
-    querystring += `${key}=${filter[key]}&`
-  }
-    return new Promise(async (resolve) =>{
-      const response = await fetch('http://localhost:8080/products?'+querystring) 
-      const data = await response.json()
-      resolve({data})
+    const categoryValues = filter[key];
+    if (categoryValues.length > 0) {
+      const lastCategoryValue = categoryValues[categoryValues.length - 1];
+      querystring += `${key}=${lastCategoryValue}&`;
     }
-    );
+   
   }
+  for (let key in sort) {
+    console.log(key);
+    querystring += `${key}=${sort[key]}&`;
+  }
+  return new Promise(async (resolve) => {
+    const response = await fetch(
+      "http://localhost:8080/products?" + querystring
+    );
+    // console.log(querystring);
+    const data = await response.json();
+    resolve({ data });
+  });
+}
 
-  // let addJob = () => {
-  //   axios
-  //     .post(`${process.env.REACT_APP_CRUD_SINGLE_JOB_URL}`, newJob, config)
-  //     .then(() => {
-  //       alert('Job Update is created')
-  //       navigate('/jobs')
-  //     })
-  //     .catch((err) => {
-  //       console.log(err)
-  //     })
-  // }
+// let addJob = () => {
+//   axios
+//     .post(`${process.env.REACT_APP_CRUD_SINGLE_JOB_URL}`, newJob, config)
+//     .then(() => {
+//       alert('Job Update is created')
+//       navigate('/jobs')
+//     })
+//     .catch((err) => {
+//       console.log(err)
+//     })
+// }
 
-  // addJob();
+// addJob();
